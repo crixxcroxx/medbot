4428a0f22538fbf65486dd35426208a6162573aa
Cris Montajes <cristajesawa@gmail.com>
2025-09-02 12:09:35 +0800

    proj structure

---
+++ template.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+mkdir -p src
+mkdir -p resource
+
+
+touch .env
+touch setup.py
+touch app.py
+
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+
+touch resource/trials.ipynb
+
+
+echo "Dir & files created"
